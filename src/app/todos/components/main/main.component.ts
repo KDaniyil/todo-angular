02a355c12d6fb29997b56a2e199d00676d33ec93
Dani <daniyil.kud@gmail.com>
2023-05-28 18:57:22 +0200

    complete project

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -12,13 +12,17 @@ export class MainComponent {
   visibleTodos$!: Observable<TodoInterface[]>;
   noTodoClass$: Observable<boolean>;
   isAllTodosSelected$: Observable<boolean>;
+  editingId: string | null = null;
+
   constructor(private todosService: TodosService) {
     this.isAllTodosSelected$ = this.todosService.todos$.pipe(
       map((todos) => todos.every((todo) => todo.isCompleted))
     );
+
     this.noTodoClass$ = this.todosService.todos$.pipe(
       map((todos) => todos.length === 0)
     );
+
     this.visibleTodos$ = combineLatest(
       this.todosService.todos$,
       this.todosService.filter$
@@ -34,8 +38,13 @@ export class MainComponent {
       })
     );
   }
+
   toggleAllTodos(event: Event) {
     const target = event.target as HTMLInputElement;
     this.todosService.toggleAll(target.checked);
+  }
+
+  setEditingId(editingId: string | null): void {
+    this.editingId = editingId;
   }
 }
--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,12 +1,69 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { TodoInterface } from '../../types/todo.interface';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit, OnChanges {
   @Input()
   todo!: TodoInterface;
+
+  @Input()
+  isEditing!: boolean;
+
+  @Output() setEditingId: EventEmitter<string | null> = new EventEmitter();
+
+  @ViewChild('textInput')
+  textInput!: ElementRef;
+
+  edititingText: string = '';
+
+  constructor(private todosService: TodosService) {}
+
+  ngOnInit(): void {
+    this.edititingText = this.todo.text;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isEditing'].currentValue) {
+      setTimeout(() => {
+        this.textInput.nativeElement.focus();
+      }, 0);
+    }
+  }
+
+  setTodoInEditMode(): void {
+    this.setEditingId.emit(this.todo.id);
+  }
+
+  removeTodo(): void {
+    this.todosService.removeTodo(this.todo.id);
+  }
+
+  toggleTodo(): void {
+    this.todosService.toggleTodo(this.todo.id);
+  }
+
+  changeText(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.edititingText = value;
+  }
+
+  changeTodo() {
+    this.todosService.changeTodo(this.todo.id, this.edititingText);
+    this.setEditingId.emit(null);
+  }
 }
